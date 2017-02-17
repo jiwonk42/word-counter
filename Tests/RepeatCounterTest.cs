aa7b274e7d1ba821a08c5ef11475524c3605be73
Jiwon Kang <jiwonk42@gmail.com>
2017-02-17 08:53:44 -0800

    fix first test method name

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -8,6 +8,13 @@ namespace RepeatCounterApp
   public class RepeatCounterTest
   {
     [Fact]
+    public void RepeatCounterCheck_ForUserInputHelloGoodbye_0()
+    {
+      RepeatCounter repeatCounterTest = new RepeatCounter();
+      Assert.Equal(0, repeatCounterTest.CounterRepeats("Hello", "Goodbye"));
+    }
+
+    [Fact]
     public void RepeatCounterCheck_ForUserInputHelloHelloGoodbye_1()
     {
       RepeatCounter repeatCounterTest = new RepeatCounter();
